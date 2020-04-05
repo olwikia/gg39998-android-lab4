79e6ffd182f8e2f841a9ec1e69a6b1387fa50817
olwikia <63187508+olwikia@users.noreply.github.com>
2020-04-05 13:10:55 +0200

    zmapowanie listy

--- app/src/main/java/com/example/gg39998_android_lab4/MainActivity.java
+++ app/src/main/java/com/example/gg39998_android_lab4/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.gg39998_android_lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] values = new String[] {"Crucail", "SanDisc", "Kingstone", "WD", "Samsung", "Seagate", "Toshiba", "Hitachi"};
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(this.adapter);
+
     }
-    //TODO
 }
