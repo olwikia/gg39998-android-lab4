14227046e721bbe2d8c7b498a7eef1a00057c650
olwikia <63187508+olwikia@users.noreply.github.com>
2020-04-05 14:13:52 +0200

    Obsługa Formularza dodawania punkt 10

--- app/src/main/java/com/example/gg39998_android_lab4/MainActivity.java
+++ app/src/main/java/com/example/gg39998_android_lab4/MainActivity.java
@@ -37,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
     public void nowyWpis(MenuItem mi){
         Intent intencja = new Intent(this, DodajWpis.class);
         startActivityForResult(intencja, 1);
-
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && requestCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+            String nowy =  (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
